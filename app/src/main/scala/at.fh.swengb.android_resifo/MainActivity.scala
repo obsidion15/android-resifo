f3242fe39a9f8b2dc1147b50bd57718de0c9f081
Martin Haring <martin.haring2@edu.fh-joanneum.at>
2016-12-25 13:53:48 +0100

    Commit for test purposes

--- app/src/main/scala/at.fh.swengb.android_resifo/MainActivity.scala
+++ app/src/main/scala/at.fh.swengb.android_resifo/MainActivity.scala
@@ -7,7 +7,7 @@ import android.os.Bundle
   * This Application helps entering data for a residence registration form.
   * The application consists of different screens which make it very easy to input the data.
   */
-
+//Testline
 class MainActivity extends AppCompatActivity {
 
   override protected def onCreate(savedInstanceState: Bundle) {
