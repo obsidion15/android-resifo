c3cffd7fc9f4f0c1f9868c11755564de08e2873e
Martin Haring <martin.haring2@edu.fh-joanneum.at>
2017-01-16 18:28:35 +0100

    Initial Implementation of OverviewActivity

---
+++ app/src/main/scala/at.fh.swengb.android_resifo/OverviewActivity.scala
@@ -0,0 +1,37 @@
+package at.fh.swengb.android_resifo
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+
+/**
+  * Created by Martin on 16.01.2017.
+  */
+class OverviewActivity extends Activity{
+
+  override protected def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.overview)
+  }
+
+  def gotoPersoenlicheDaten(view:View): Unit ={
+    val i = new Intent(this, classOf[PersoenlicheDatenActivity])
+    startActivity(i)
+  }
+
+  def gotoAnmeldung(view:View): Unit ={
+    val i = new Intent(this, classOf[AnmeldungActivity])
+    startActivity(i)
+  }
+
+  def gotoHauptwohnsitz(view:View): Unit ={
+    val i = new Intent(this, classOf[HauptwohnsitzActivity])
+    startActivity(i)
+  }
+
+  def gotoAbmeldung(view:View): Unit ={
+    val i = new Intent(this, classOf[AbmeldungActivity])
+    startActivity(i)
+  }
+}
