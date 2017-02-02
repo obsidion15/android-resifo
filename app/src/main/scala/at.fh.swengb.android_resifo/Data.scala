bc5f039f2e1fbb919dea4e261c46e46e5073e6f4
Sabine Gollinger <sabinegollinger@msn.com>
2017-02-02 10:39:40 +0100

    Liste vorbereitet

--- app/src/main/scala/at.fh.swengb.android_resifo/Data.scala
+++ app/src/main/scala/at.fh.swengb.android_resifo/Data.scala
@@ -1,6 +1,9 @@
 package at.fh.swengb.android_resifo
 
 import android.database.Cursor
+import android.widget.EditText
+import android.app.Activity
+import android.view.View
 
 /**
   * Created by Martin on 20.01.2017.
@@ -14,11 +17,57 @@ class Data {
     fillAbmeldeDaten(db, i)
   }
 
+  def getDataIntoList(db: Db):Map[Int, List[Any]] = {
+    var someCursor: Option[Cursor] = None
+    var someCursorHw: Option[Cursor] = None
+    var dataMap: Map[Int, List[Any]] = Map()
+    var person_id: Int = 0
+    var nachname: String = ""
+    var vorname: String = ""
+    var hauptsitz_id: Int = 0
+    var strasse: String = ""
+    var hausnr: String = ""
+    var plz: String = ""
+    var ort: String = ""
+    var bundesland: String = ""
+
+    try {
+      someCursor = Option(db.getReadableDatabase.query("person", Array("person_id", "nachname", "vorname"), null, null, null, null, null))
+      someCursor match {
+        case None => System.err.println("Could not execute query due to some reason")
+        case Some(c) =>
+          while (c.moveToNext()) {
+            person_id = c.getInt(c.getColumnIndex("person_id"))
+            nachname = c.getString(c.getColumnIndex("nachname"))
+            vorname = c.getString(c.getColumnIndex("vorname"))
+
+            someCursorHw = Option(db.getReadableDatabase.query("hauptsitz", Array("hauptsitz_id", "person_id", "strasse", "hausnr", "plz", "ort", "bundesland"), "person_id = " + c.getInt(c.getColumnIndex("person_id")), null, null, null, null))
+            someCursorHw match {
+              case None => System.err.println("Could not execute query due to some reason")
+              case Some(cHw) =>
+                while (cHw.moveToNext()) {
+                  hauptsitz_id = c.getInt(c.getColumnIndex("hauptsitz_id"))
+                  strasse = c.getString(c.getColumnIndex("strasse"))
+                  hausnr = c.getString(c.getColumnIndex("hausnr"))
+                  plz = c.getString(c.getColumnIndex("plz"))
+                  ort = c.getString(c.getColumnIndex("ort"))
+                  bundesland = c.getString(c.getColumnIndex("bundesland"))
+                }
+            }
+            dataMap = dataMap + (person_id -> List(nachname, vorname, strasse, hausnr, plz, ort, bundesland))
+          }
+      }
+      return dataMap
+    } finally {
+      someCursor foreach (_.close())
+      Map()
+    }
+  }
+
   def fillPersDaten(db: Db, i: Int): Unit = {
     var someCursor: Option[Cursor] = None
     try {
       someCursor = Option(db.getReadableDatabase.query("person", Array("person_id", "nachname", "vorname", "nachnameAlt", "geburtsdatum", "geburtsort", "geschlecht", "religion", "familienstand", "staatsangehoerigkeit"), null, null, null, null, null))
-
       someCursor match {
         case None => System.err.println("Could not execute query due to some reason")
         case Some(c) =>
